7bfc72a8ed2274d16ae60330f3c6f1912a7686bc
Ashish Agarwal <ashishag07@gmail.com>
2025-06-30 11:01:42 +0530

    removed unwanted files, modified app and index file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ComparePage from "./pages/ComparePage";
+import DashboardPage from "./pages/DashboardPage";
+import { BrowserRouter, Route, Routes } from "react-router";
+import WatchlistPage from "./pages/WatchlistPage";
+import HomePage from "./pages/HomePage";
+import CoinPage from "./pages/CoinPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/watchlist" element={<WatchlistPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
